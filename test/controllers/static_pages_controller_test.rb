477199bf9f77628c2b2e06fea16a61557fda17c9
imhta <imhtapm@gmail.com>
2019-08-15 22:48:56 +0530

    link addded
    link test init
    rubocop -a

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,30 +1,31 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "SampleRailsApp"
+    @base_title = 'SampleRailsApp'
   end
-  test "should get root" do
+  test 'should get root' do
     get root_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
-  test "should get home" do
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
 
-  test "should get help" do
+  test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
-  test "should get about" do
+  test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select 'title', "About | #{@base_title}"
   end
-
 end
