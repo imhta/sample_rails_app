0187619bf2f0c6b54b4f873d7e6661a67d654533
imhta <imhtapm@gmail.com>
2019-08-23 22:01:49 +0530

    error fixed in error style

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,4 +4,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+  end
 end
