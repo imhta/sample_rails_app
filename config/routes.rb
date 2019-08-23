8b9bc98187dbd8cc2aedee367d1ce9d188261c31
imhta <imhtapm@gmail.com>
2019-08-23 19:11:59 +0530

    user resources added to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help', as: 'help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
+  get  '/signup',  to: 'users#new' 
+  resources :users
 end
