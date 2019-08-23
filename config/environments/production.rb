fb76e393d1a55966cafdf6ddc2411b166a6b957f
imhta <imhtapm@gmail.com>
2019-08-23 22:16:05 +0530

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   #   logger.formatter = config.log_formatter
   #   config.logger    = ActiveSupport::TaggedLogging.new(logger)
   # end
-
+  config.force_ssl = true
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
