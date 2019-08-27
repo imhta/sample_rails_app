2b19fb165480bf37d27f4cd1a11a1c9e0cfb318a
imhta <imhtapm@gmail.com>
2019-08-27 21:30:35 +0530

    redirect after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       # Create an error message.
