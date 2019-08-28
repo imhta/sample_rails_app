cfee72b4174e8f5aec06495b8e865551f39b0dd5
imhta <imhtapm@gmail.com>
2019-08-28 15:16:15 +0530

    unexpected end fixed in user model for class << self

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,18 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 6 }
   class << self
       # Returns the hash digest of the given string.
-  def digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  def new_token
-    SecureRandom.urlsafe_base64
-  end
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
   end
 end
