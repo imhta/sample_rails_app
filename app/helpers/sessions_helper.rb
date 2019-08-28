d1474648501bdeb2babe177398ab823fa774da64
imhta <imhtapm@gmail.com>
2019-08-28 15:18:47 +0530

    remember user to session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,11 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+      end
     def current_user
         if session[:user_id]
           @current_user ||= User.find_by(id: session[:user_id])
