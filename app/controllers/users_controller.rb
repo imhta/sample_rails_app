68e66db0072f5df3508c553326165d38c581ff85
imhta <imhtapm@gmail.com>
2019-08-28 20:07:32 +0530

    before action added to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
