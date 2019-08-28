711cac4c0cfffb2e36e48b6cd49c98da4efc8c36
imhta <imhtapm@gmail.com>
2019-08-28 15:10:44 +0530

    rename User to self in User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,13 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 6 }
 
       # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
   def remember
