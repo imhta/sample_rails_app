cf2a17925d64a1d6629eb5b8695ddced0af73558
imhta <imhtapm@gmail.com>
2019-08-28 20:39:43 +0530

    should redirect index when not logged in added user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user       = users(:hi)
+    @other_user = users(:michael)
+  end
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
   test 'should get new' do
     get signup_path
     assert_response :success
