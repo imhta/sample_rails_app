839e6af0c66682f9aa14b4a0202075720d17de33
imhta <imhtapm@gmail.com>
2019-08-28 15:13:33 +0530

    shorthand for self by singleton class << self

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-
+  class << self
       # Returns the hash digest of the given string.
-  def self.digest(string)
+  def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def self.new_token
+  def new_token
     SecureRandom.urlsafe_base64
   end
   def remember
