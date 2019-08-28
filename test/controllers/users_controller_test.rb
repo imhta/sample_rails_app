14a20f415544f741af6833687bedc25537c76a07
imhta <imhtapm@gmail.com>
2019-08-28 21:47:42 +0530

    should not allow the admin attribute to be edited via the web test added

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,6 +11,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              FILL_IN,
+                                            password_confirmation: FILL_IN,
+                                            admin: FILL_IN } }
+    assert_not @other_user.FILL_IN.admin?
+  end
   test 'should get new' do
     get signup_path
     assert_response :success
