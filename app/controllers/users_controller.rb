effa78e0e457c2f8c1e3a1796ccd85fb016a2a22
imhta <imhtapm@gmail.com>
2019-08-28 20:05:49 +0530

    logged in user method to private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,4 +38,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
