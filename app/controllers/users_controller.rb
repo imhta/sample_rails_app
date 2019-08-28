5c71a37ce7d8c803cb909fa90d019f64aa378727
imhta <imhtapm@gmail.com>
2019-08-28 20:01:23 +0530

    flash message added to update of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
