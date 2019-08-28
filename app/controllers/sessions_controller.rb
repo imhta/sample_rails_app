3e0476bd34baca0836e02408ef70181f41949da8
imhta <imhtapm@gmail.com>
2019-08-28 15:39:52 +0530

    mild change in remember me condition

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      params[:session][:remember_me] ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
