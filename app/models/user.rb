db5abbef2a2c67e1ac0fbaba7050b6636f017be4
imhta <imhtapm@gmail.com>
2019-08-29 22:23:21 +0530

    A template for using update_columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,8 +40,7 @@ class User < ApplicationRecord
   end
   # Activates an account.
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: FILL_IN, activated_at: FILL_IN)
   end
 
   # Sends activation email.
