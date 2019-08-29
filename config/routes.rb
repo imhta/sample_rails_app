5b5f428cff0680ad5850c05acee54720636de98c
imhta <imhtapm@gmail.com>
2019-08-29 21:57:42 +0530

    A working preview method for account activation.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   get 'sessions/new'
+  default_url_options :host => "localhost:3000"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
