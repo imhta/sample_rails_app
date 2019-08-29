947add4032a8f53a610799bae731de652b33dd93
imhta <imhtapm@gmail.com>
2019-08-29 22:17:37 +0530

    Adding account activation to the user signup test.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,16 +19,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-  test 'valid signup information' do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: 'Example User',
+      post users_path, params: { user: { name:  "Example User",
                                          email: 'user@example.com',
                                          password: 'password',
                                          password_confirmation: 'password' } }
     end
     follow_redirect!
-    # assert_template 'users/show'
-    # assert is_logged_in?
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
