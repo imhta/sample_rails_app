58c84f46251479daed6c5cfad48240d5aba6f0ae
imhta <imhtapm@gmail.com>
2019-08-30 17:04:23 +0530

    The edit action for password reset.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
   def new
   end
   def create
@@ -14,5 +16,19 @@ class PasswordResetsController < ApplicationController
     end
   end
   def edit
+  end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    unless (@user && @user.activated? &&
+            @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
   end
 end
