93e90055124521dbd95f0783963e24cd1c3b75cf
imhta <imhtapm@gmail.com>
2019-08-30 16:50:58 +0530

    activation bug fix

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,7 +4,6 @@ class AccountActivationsController < ApplicationController
     def edit
         user = User.find_by(email: params[:email])
         if user && !user.activated? && user.authenticated?(:activation, params[:id])
-          user.activate
           user.update_attribute(:activated,    true)
           user.update_attribute(:activated_at, Time.zone.now)
           log_in user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    redirect_to root_url 
   end
 
   def new
