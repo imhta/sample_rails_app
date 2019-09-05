fdda2fe89373328e2e267df4d99693f8f46d8940
imhta <imhtapm@gmail.com>
2019-09-05 21:35:43 +0530

    Adding a feed instance variable to the home action

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,10 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   private
 
   # Converts email to all lower-case.
