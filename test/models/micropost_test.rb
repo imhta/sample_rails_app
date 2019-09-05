4e32bc36716efdf2dca1fad55a1e39454616e762
imhta <imhtapm@gmail.com>
2019-09-05 20:17:57 +0530

    Using idiomatically correct code to build a micropost.

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # This code is not idiomatically correct.
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
