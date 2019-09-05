34e43537437a2038b6bcf6f49ff065835e3827ff
imhta <imhtapm@gmail.com>
2019-09-05 20:17:12 +0530

    Sets the password reset attributes updated fill in

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(reset_digest:  FILL_IN, reset_sent_at: FILL_IN)
+    update_columns(reset_digest:  self.reset_token, reset_sent_at: Time.now)
   end
 
   # Sends password reset email.
