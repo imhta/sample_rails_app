e95e28172d9a0b904cdd0c4e5191f9ee0280faf7
imhta <imhtapm@gmail.com>
2019-09-05 20:55:39 +0530

    _micro post style improved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts
     redirect_to root_url and return unless @user.activated
   end
 
