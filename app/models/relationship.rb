0d63a04ecb95d954f1ea92380640677789bc0d99
imhta <imhtapm@gmail.com>
2019-09-15 13:19:28 +0530

    rename attr accessor

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Relationship < ApplicationRecord
-  attr_accessible :followed_id
+  attr_accessor :followed_id
 
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
