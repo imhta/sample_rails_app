f8fc5a16e37e7716fed3c780a82f69b7959b5e89
imhta <imhtapm@gmail.com>
2019-09-15 11:43:19 +0530

    Implementing user.followers using reverse relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
 
+  has_many :reverse_relationships, foreign_key: 'followed_id',
+                                   class_name: 'Relationship',
+                                   dependent: :destroy
+  
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
