d6bfd7fad7ba78b7e234e99fe785a0a447148ef5
imhta <imhtapm@gmail.com>
2019-09-15 11:20:07 +0530

    snakecase for follower_id and followed_id

--- db/migrate/20190915054438_create_relationships.rb
+++ db/migrate/20190915054438_create_relationships.rb
@@ -1,15 +1,13 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
-      t.string :follower
-      t.integer :id
-      t.string :followed
-      t.integer :id
+      t.string :follower_id
+      t.string :followed_id
 
       t.timestamps
     end
-    add index :relationships, :follower id
-    add index :relationships, :followed id
-    add index :relationships, [:follower id, :followed id], unique: true
+    add index :relationships, :follower_id
+    add index :relationships, :followed_id
+    add index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
