b7ea15686ec84b23f797b96b621cd1ccd2eeccd1
imhta <imhtapm@gmail.com>
2019-09-15 11:59:38 +0530

    Adding followed_users and followers actions to the Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,9 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: %i[new create edit update]
   resources :microposts,          only: %i[create destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
