7366dadfdc903cb5c9e85deb64e1854023a5a349
imhta <imhtapm@gmail.com>
2019-09-15 11:39:17 +0530

    Unfollowing a user by destroying a user relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -82,6 +82,10 @@ class User < ApplicationRecord
     relationships.create!(followed_id: other_user.id)
   end
 
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
   private
 
   # Converts email to all lower-case.
