412a79be596d4319b5c4b89a2f599e4cc23a13ae
imhta <imhtapm@gmail.com>
2019-09-15 11:55:32 +0530

    Adding following/follower relationships to the sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,10 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+users = User.all
+user = users.first
+followed_users = users[2..50]
+followers = users[3..40]
+followed_users.each { |followed| user.follow!(followed) }
+followers.each { |follower| follower.follow!(user) }
