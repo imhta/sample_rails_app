e47d1a4a88f888f9b7cbd684127fa746eb54e510
imhta <imhtapm@gmail.com>
2019-09-15 12:50:03 +0530

    The following and followers actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,18 @@ class UsersController < ApplicationController
     flash[:success] = 'User deleted'
     redirect_to users_url
   end
-
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render "show_follow"
+  end
+  def followed_users
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
   private
 
   def user_params
